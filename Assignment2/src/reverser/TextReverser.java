97cdf8ad97da5d1c65a49b213217160f1575eb53
Jim Frode Nilsen <jimnil93@hotmail.com>
2015-03-02 20:06:02 +0100

    v0.3

--- Assignment2/src/reverser/TextReverser.java
+++ Assignment2/src/reverser/TextReverser.java
@@ -2,16 +2,27 @@ package reverser;
 
 public class TextReverser {
 	static int numberOfCharacters = 0;
+	
 	public static void reverse(String text){
 		System.out.print(text.charAt(text.length()-1));
 		numberOfCharacters++;
 		if(text.length() > 1){
-			text = text.substring(0, text.length()-1);
-			reverse(text);
+			reverse(text, text.length()-1);
 		}else{
 			System.out.println("\nAntall tegn: " + numberOfCharacters);
 			numberOfCharacters = 0;
 		}
 		
 	}
+	public static void reverse(String text, int last){
+		System.out.print(text.charAt(last-1));
+		numberOfCharacters++;
+		if(last > 1){
+			reverse(text, last-1);
+		}else{
+			System.out.println("\nAntall tegn: " + numberOfCharacters);
+			numberOfCharacters = 0;
+		}
+	}
 }
+
